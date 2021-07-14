440148f397c6a93a2cc60c30b8538676e8780f49
Josef Leyva <jalm_x@hotmail.com>
2021-07-14 14:01:54 -0500

    miercoles

---
+++ Codigo/Semana2/03miercoles/funcion1.py
@@ -0,0 +1,23 @@
+
+def saludo():
+    '''
+        Esta funcion manda un mensaje a los unicornios nordicos amarillos
+    '''
+    print("Hola estoy programando en Python")
+
+def al_cuadrado(x):
+    '''
+    Eleva a cuadrado x
+    '''
+    cuadrado = x ** 2
+    print( f"El cuadrado de {x} es {cuadrado}" )
+
+def suma(a, b):
+    print(a+b)
+
+saludo()
+
+al_cuadrado(3)
+
+suma(5,3)
+
