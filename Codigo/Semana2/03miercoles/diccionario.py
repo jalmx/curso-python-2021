a927461ba5f6fd089a260a1447a03f3f3196f65a
Josef Leyva <jalm_x@hotmail.com>
2021-07-14 13:32:42 -0500

    enumerate

--- Codigo/Semana2/03miercoles/diccionario.py
+++ Codigo/Semana2/03miercoles/diccionario.py
@@ -33,3 +33,46 @@ print(camiones)
 # golosina3 : gansito
 # agregar 2 elementos mas, que tu quieras
 # imprimir el diccionario
+
+for key in autos:
+    print(f'La llave {key} tiene el valor: {autos[key]}')
+
+print('=====================')
+
+for key in autos.keys():
+    print(key)
+
+print('=====================')
+
+for valor in autos.values():
+    print(valor)
+
+print('=====================')
+
+lista = list() # lista vacia
+lista2 = []  # lista vacia
+
+tupla = tuple() #tupla vacia
+tupla2 = () # tupla vacia
+
+dicc = dict() # diccionario vacio
+dicc2 = {} # diccionario vacio
+
+print('=================================')
+
+numeros = [1,58,6,9,3,4,6,1,69,1,61,54]
+
+indice, valor = (1, 25) # destructuracion
+indice, valor, otro  = 25 , 65, 85 # multi asignacion
+
+print(indice)
+print(valor)
+print(otro)
+
+for indice, valor in enumerate(numeros):
+    print(f'En el {indice} tiene el valor {valor}')
+
+print('=======================================')
+
+for index, key in enumerate(autos):
+    print(f'En el {index} tiene el valor {key}')
